4f88f2c9ac8ceb515a58dfb2224ccbab60cd7f6b
Martin Burger <mburger82@yahoo.de>
2017-12-18 18:47:17 +0100

    add log library. Not yet finished. printf style string handling alredy works. UART sending has to be added.

---
+++ FreeRTOSTest1/include/log.h
@@ -0,0 +1,16 @@
+/*
+ * log.h
+ *
+ * Created: 18.12.2017 18:01:57
+ *  Author: Martin Burger
+ */ 
+
+
+#ifndef LOG_H_
+#define LOG_H_
+
+
+int edulog(char const *fmt, ...);
+
+
+#endif /* LOG_H_ */
